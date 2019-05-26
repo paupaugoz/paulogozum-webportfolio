59440cb823491e8d9cad23c99474f46aa02d94af
paupaugoz <paugozum1@gmail.com>
2019-05-26 14:41:47 +0800

    Updates

---
+++ precache-manifest.2ee11865ef353ae1f9964d3bf6ef4538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0f9c8ac2a919ed5529bec2f79e4cca",
+    "url": "/react-ghpages/index.html"
+  },
+  {
+    "revision": "004fc0264382e0b1bc4f",
+    "url": "/react-ghpages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-ghpages/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "004fc0264382e0b1bc4f",
+    "url": "/react-ghpages/static/js/main.707b8aef.chunk.js"
+  },
+  {
+    "revision": "7bb21b4534daeb8b4d81",
+    "url": "/react-ghpages/static/js/runtime~main.47cdb9c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ghpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-ghpages/precache-manifest.2ee11865ef353ae1f9964d3bf6ef4538.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ghpages/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
