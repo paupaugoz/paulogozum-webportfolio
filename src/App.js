2d346fe37264f3e67f1a21bbf785d3bd928208a3
paupaugoz <paugozum1@gmail.com>
2019-05-26 14:58:47 +0800

    Create React app for web portfolio

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Components/Introduction.css'
+import Introduction from './Components/Introduction'
+
+function App() {
+  return (
+      <div>
+      <Introduction />
+
+      </div>    
+  );
+}
+
+export default App;
