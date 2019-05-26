c94e390920fa5273e609343e0770bafb4b4e696b
paupaugoz <paugozum1@gmail.com>
2019-05-26 16:35:52 +0800

    Updates

--- precache-manifest.203e08af01ae500a45f6245bdec2c7f1.js
+++ precache-manifest.203e08af01ae500a45f6245bdec2c7f1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6bfb55c8e7608ca28bc191f5425d5e4e",
+    "revision": "b882dd7af041b1b8506d050541f0d0dd",
     "url": "/paulogozum-webportfolio/index.html"
   },
   {
-    "revision": "5a4d0e542023ff46d0b5",
+    "revision": "aff7c99ea5f51fe96316",
     "url": "/paulogozum-webportfolio/static/css/main.3afb302f.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/paulogozum-webportfolio/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "5a4d0e542023ff46d0b5",
-    "url": "/paulogozum-webportfolio/static/js/main.e7383395.chunk.js"
+    "revision": "aff7c99ea5f51fe96316",
+    "url": "/paulogozum-webportfolio/static/js/main.01d07c4d.chunk.js"
   },
   {
     "revision": "9e990e806d6ee9742d0a",
