070711e6c63ccea44c67140e24fd9fe97460efb4
paupaugoz <paugozum1@gmail.com>
2019-05-31 23:32:51 +0800

    Mac switch

--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -9,18 +9,22 @@ export default class Introduction extends Component {
     return (
       <div id="introduction" className="intro-wrapper">
 
-        <div className="image-container">
+       {/*} <div className="image-container">
           <img src="./whitelogo.png" className="animated fadeInDown img" alt="Logo" />
         </div>
-
+      */}
+      
         <div className="wrapper">
 
           <div className="flex-wrapper ">
-
-            <h1 className="animated fadeInDownBig offwhite test">Hi I'm Gozum</h1>
+            
+            <div className="image-container">
+              <img src="./whitelogo.png" className="animated fadeInDown img" alt="Logo" />
+            </div>
+            <h1 id="name-header" className="animated fadeInDownBig offwhite">Hi I'm Gozum</h1>
             <h1 className="animated fadeInDown delay-1s red raleway">I am a front-end web developer.</h1>
-            <h1 className="animated fadeInDown delay-2s offwhite raleway">Ad Majorem Dei Gloriam</h1>
-            <h1 className="animated fadeInDown delay-3s red raleway">Magis</h1>
+            <h1 className="animated fadeInDown delay-2s blue raleway">Ad Majorem Dei Gloriam</h1>
+            <h1 id="last-header" className="animated fadeInDown delay-3s red raleway">Magis</h1>
           </div>
 
         </div>
