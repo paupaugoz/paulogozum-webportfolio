515def6cce7ff8f58aece7ab26cb1e9f62abfcee
paupaugoz <paugozum1@gmail.com>
2019-06-06 06:55:42 -0700

    Fix format

--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -1,44 +1,24 @@
 import React, { Component } from 'react'
-import { AnimatedText }  from './AnimatedText';
-import artboard from './whitelogo.png';
 
-
-
-export default class Introduction extends Component {
-  render () {
+export default class Projects extends Component {
+  render() {
     return (
-      <div id="introduction" className="intro-wrapper">
-      
-         {/*} <div className="image-container">
-          <img src="./whitelogo.png" className="animated fadeInDown img" alt="Logo" />
-        </div>
-       */}
-        <div id="header">
-        </div>
-        <div className="wrapper">
-
-          <div className="flex-wrapper">
-
-            <div className="image-container">
-              <img src="./blacklogo.png" className="animated fadeInDown img" alt="Logo" />
-            </div>
-            <h1 id="name-header" className="animated fadeInDownBig heading red">Code That Works</h1>
-            <h2 className="animated fadeInDown delay-1s black ">Hi I'm Gozum</h2>
-            <h3 className="animated fadeInDown delay-2s black ">I'm a <span className="red">front end web developer </span>
+      <div id="intro-wrapper">
+        <div id="header"></div>
+        <div id="main-wrapper">
+          <div className="image-container">
+            <img src="./blacklogo.png" className="animated fadeInDown img" alt="Logo" />
+          </div>
+          <h1 id="name-header" className="animated fadeInDownBig red">Code That Works</h1>
+          <h2 className="animated fadeInDown delay-1s black ">Hi I'm Gozum</h2>
+          <h3 className="animated fadeInDown delay-2s black ">I'm a <span className="red">front end web developer </span>
             based in Southern California</h3>
-            <h3 id="last-header" className="animated fadeInDown delay-3s black ">My goal is to 
+          <h3 id="last-header" className="animated fadeInDown delay-3s black ">My goal is to
             <span className="red"> align technology </span> with <span className="red">business
             objectives</span></h3>
-
-
-                 
-          </div>
-
-
         </div>
-        <div id="footer">
+        <div id="footer"><h1 className="red"></h1></div>
 
-        </div>
       </div>
 
     )
--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -3,10 +3,22 @@ import React, { Component } from 'react'
 export default class Projects extends Component {
   render() {
     return (
-      <div className="projects-wrapper blue big">
-      <div className="test-wrapper">
-       <h1 className="animated fadeInDownBig offwhite test">Hi I'm Gozum</h1>         
-      </div>
+      <div className="intro-wrapper">
+        <div id="header"></div>
+        <div id="main-wrapper">
+          <div className="image-container">
+            <img src="./blacklogo.png" className="animated fadeInDown img" alt="Logo" />
+          </div>
+          <h1 id="name-header" className="animated fadeInDownBig red">Code That Works</h1>
+          <h2 className="animated fadeInDown delay-1s black ">Hi I'm Gozum</h2>
+          <h3 className="animated fadeInDown delay-2s black ">I'm a <span className="red">front end web developer </span>
+            based in Southern California</h3>
+          <h3 id="last-header" className="animated fadeInDown delay-3s black ">My goal is to
+            <span className="red"> align technology </span> with <span className="red">business
+            objectives</span></h3>
+        </div>
+        <div id="footer"><h1 className="red"></h1></div>
+
       </div>
 
     )
