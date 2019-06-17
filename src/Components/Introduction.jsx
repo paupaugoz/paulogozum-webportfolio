fc9d577e0ec0a28071189675f2d70ee43019a081
paupaugoz <paugozum1@gmail.com>
2019-06-17 14:03:40 +0800

    Update introduction.jsx

--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -5,15 +5,16 @@ export default class Projects extends Component {
   render() {
     return (
       <div id="intro-wrapper">
+      <div class="animated fadeIn">
         <div id="header">
         <div class="animated-container">
-        <div class="animated fadeIn">
+        <div class="animated fadeIn slower">
         <AnimatedText  >
         PAULO GOZUM
       </AnimatedText>
       </div>
         </div>
-        <div class="animated fadeIn  icon-container">
+        <div class="animated fadeIn slower icon-container">
           <div className="icon-wrapper">
             <a href="https://www.linkedin.com/in/jan-paulo-gozum-b563a5172/"><span class="fab fa-linkedin"></span></a>
           </div>
@@ -29,15 +30,16 @@ export default class Projects extends Component {
 
         
         </div>
+        </div>
        
         <div id="main-wrapper">
           <div className="image-container">
-            <img src="./blacklogo.png" className="animated fadeIn delay-1s img" alt="Logo" />
+            <img src="./blacklogo.png" className="animated fadeIn slower img" alt="Logo" />
           </div>
-          <h1 id="name-header" className="animated fadeInDown delay-2s red">Hi I'm Gozum</h1>
-          <h2 className="animated fadeInDown delay-2s black ">I'm a <span className="red">front end web developer </span>
+          <h1 id="name-header" className="animated fadeInDown red">Hi I'm Gozum</h1>
+          <h2 className="animated fadeInDown delay-1s black ">I'm a <span className="red">front end web developer </span>
             based in Southern California</h2>
-          <h2 id="last-header" className="animated fadeInDown delay-3s black ">My goal is to
+          <h2 id="last-header" className="animated fadeInDown delay-2s black ">My goal is to
             <span className="red"> align technology </span> with <span className="red">business
             objectives</span></h2>
         </div>
